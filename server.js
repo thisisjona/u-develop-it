77a5f2cbefe73f11650e97e9ea8dbb3e973499b7
thisisjona <thisisjona@gmail.com>
2022-03-15 14:17:39 -0600

    12.2.3

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,16 +9,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.json({
-        message: 'Hello World!'
-    })
-});
-
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'wcwvswwF1!',
+        database: 'election'
+        
+    },
+    console.log('Connected to the election database')
+)
 //Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
